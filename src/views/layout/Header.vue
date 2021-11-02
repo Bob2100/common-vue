8fc99d2d49b02e442697f9704a5de2ab44fa915a
Bob <bob_w@qq.com>
2021-11-02 19:11:59 +0800

    logo的样式

--- src/views/layout/Header.vue
+++ src/views/layout/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="Header">
-    Header
+    <a class="navbar-brand" href="#">前端二牛</a>
   </div>
 </template>
 
@@ -9,3 +9,22 @@ export default {
   name: "Header"
 };
 </script>
+<style scoped lang="less">
+.navbar-brand {
+  font-size: 24px;
+  height: 53 px;
+  font-weight: 700;
+  margin-right: 0;
+  color: #fff;
+  display: inline-block;
+  &:hover {
+    color: #1abc9c;
+    background-color: transparent;
+  }
+}
+a {
+  color: #16a085;
+  text-decoration: none;
+  transition: 0.25s;
+}
+</style>
