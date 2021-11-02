f8c695ba6470b78b4ace2da815973758c202cc99
Bob <bob_w@qq.com>
2021-11-02 21:26:43 +0800

    添加clone示意图

--- src/views/chartDemo/DemoLineChart.vue
+++ src/views/chartDemo/DemoLineChart.vue
@@ -1,12 +1,22 @@
 <template>
-  <div class="DemoLineChart chart-wrapper">
-    <ChartTrend title-text="图A" :x-data="xData" :y-data="yData.debtRatio" />
-    <ChartTrend title-text="图B" :x-data="xData" :y-data="yData.flowRatio" />
-    <ChartTrend title-text="图C" :x-data="xData" :y-data="yData.profitRatio" />
-    <div class="chart-panel">
-      <img :src="erniuIcon">
-      <h4>欢迎扫码关注二牛的公众号 ：）</h4>
-      <a href="https://github.com/Bob2100/common-vue" target="_blank">本站源码</a>
+  <div>
+    <div class="DemoLineChart chart-wrapper">
+      <ChartTrend title-text="图A" :x-data="xData" :y-data="yData.debtRatio" />
+      <ChartTrend title-text="图B" :x-data="xData" :y-data="yData.flowRatio" />
+      <ChartTrend title-text="图C" :x-data="xData" :y-data="yData.profitRatio" />
+      <div class="chart-panel">
+        <img :src="erniuIcon">
+        <h4>欢迎扫码关注二牛的公众号 ：）</h4>
+      </div>
+    </div>
+    <div>
+      <p>
+        <a href="https://github.com/Bob2100/common-vue" target="_blank">本站源码</a>
+      </p>
+      <p>打开 git bash 键入如下命令并回车：</p>
+      <p>git clone git@github.com:Bob2100/common-vue.git</p>
+      <p>如下图所示：</p>
+      <p><img style="width:100%;display:inline-block" :src="cloneImg"></p>
     </div>
   </div>
 </template>
@@ -14,6 +24,7 @@
 <script>
 import ChartTrend from './common/ChartTrend.vue';
 import erniuIcon from '../../assets/erniu.jpeg';
+import cloneImg from '../../assets/clone.png';
 
 export default {
   name: 'DemoLineChart',
@@ -28,6 +39,9 @@ export default {
   computed: {
     erniuIcon() {
       return erniuIcon;
+    },
+    cloneImg() {
+      return cloneImg;
     },
     xData() {
       return ['2018', '2019', '2020'];
