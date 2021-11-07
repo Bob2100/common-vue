564ff093992cb8639b7d25319a09d548b4279b24
Bob <bob_w@qq.com>
2021-11-07 20:15:21 +0800

    添加动画

--- src/views/layout/Main.vue
+++ src/views/layout/Main.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="Main">
-    <router-view />
+    <transition name="fade" mode="out-in">
+      <router-view />
+    </transition>
   </div>
 </template>
 
@@ -9,3 +11,13 @@ export default {
   name: "Main"
 };
 </script>
+<style scoped lang="less">
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
