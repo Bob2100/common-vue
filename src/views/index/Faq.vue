0b722c41b177bedb8574cdbc4d64d2e86faf720e
Bob <bob_w@qq.com>
2021-11-07 15:07:34 +0800

    文本样式

--- src/views/index/Faq.vue
+++ src/views/index/Faq.vue
@@ -5,7 +5,7 @@
         <a href="https://github.com/Bob2100/common-vue" target="_blank">本站源码</a>
       </h3>
       <p>打开 git bash 键入如下命令并回车：</p>
-      <p>git clone git@github.com:Bob2100/common-vue.git</p>
+      <p><i><strong>git clone</strong> git@github.com:Bob2100/common-vue.git</i></p>
       <p>如下图所示：</p>
       <p><img class="common-img" src="../../assets/clone.png"></p>
     </article>
