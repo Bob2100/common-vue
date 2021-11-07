d1830439111d7897c1d1a57ec8de53d1dceb421b
Bob <bob_w@qq.com>
2021-11-07 16:42:57 +0800

    bo-navbar传递v-model

--- src/views/layout/Header.vue
+++ src/views/layout/Header.vue
@@ -3,16 +3,16 @@
     <a class="navbar-brand" href="javascript:;"
       @click="$router.push({name:'Layout'})">前端二牛</a>
     <div class="navbar-collapse">
-      <bo-navbar>
-        <bo-navbar-item>
+      <bo-navbar v-model="activeItem">
+        <bo-navbar-item name="git">
           <a href="javascript:;"
             @click="$router.push({name:'GitTeach'})">Git常用命令</a>
         </bo-navbar-item>
-        <bo-navbar-item>
+        <bo-navbar-item name="article">
           <a href="https://mp.weixin.qq.com/s/TDxxvnu6qkvOJWcQmXbqew"
             target="_blank">看文章</a>
         </bo-navbar-item>
-        <bo-navbar-item>
+        <bo-navbar-item name="version">
           <a href="https://github.com/Bob2100/common-vue/tags"
             target="_blank">{{version}}</a>
         </bo-navbar-item>
@@ -24,6 +24,11 @@
 <script>
 export default {
   name: "Header",
+  data() {
+    return {
+      activeItem: 'version'
+    }
+  },
   computed: {
     version() {
       return `v${process.env.VUE_APP_VERSION}`;
