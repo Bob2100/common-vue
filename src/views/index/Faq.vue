149de6583b8059b2a1059a6132ff68ea44b2e15b
Bob <bob_w@qq.com>
2021-11-07 15:03:56 +0800

    增加FAQ

--- src/views/index/Faq.vue
+++ src/views/index/Faq.vue
@@ -1,14 +1,19 @@
 <template>
   <div class="Faq">
-    <p>
-      <a href="https://github.com/Bob2100/common-vue" target="_blank">本站源码</a>
-    </p>
-    <p>打开 git bash 键入如下命令并回车：</p>
-    <p>git clone git@github.com:Bob2100/common-vue.git</p>
-    <p>如下图所示：</p>
-    <p><img class="common-img" src="../../assets/clone.png"></p>
-    <p><img class="common-img width-middle"
-        src="../../assets/imgs/clone-https.png"></p>
+    <article>
+      <h3>
+        <a href="https://github.com/Bob2100/common-vue" target="_blank">本站源码</a>
+      </h3>
+      <p>打开 git bash 键入如下命令并回车：</p>
+      <p>git clone git@github.com:Bob2100/common-vue.git</p>
+      <p>如下图所示：</p>
+      <p><img class="common-img" src="../../assets/clone.png"></p>
+    </article>
+    <article>
+      <h3>FAQ</h3>
+      <p><img class="common-img width-middle"
+          src="../../assets/imgs/clone-https.png"></p>
+    </article>
   </div>
 </template>
 
