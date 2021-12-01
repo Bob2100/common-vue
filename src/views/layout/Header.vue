b1e9c3bf95cbce881a7e42e820e11351748f9263
Bob <bob_w@qq.com>
2021-12-01 17:31:56 +0800

    重构，分离出MenuPc组件

--- src/views/layout/Header.vue
+++ src/views/layout/Header.vue
@@ -1,22 +1,6 @@
 <template>
   <div class="Header">
-    <el-menu :default-active="activeIndex" class="header-menu" mode="horizontal"
-      @select="handleSelect" background-color="#34495e" text-color="#fff"
-      active-text-color="#1abc9c">
-      <el-menu-item index="index">
-        <span class="logo" @click="$router.push({name:'Layout'})">前端二牛</span>
-      </el-menu-item>
-      <el-menu-item index="GitTeach" class="header-menu-item">Git常用命令
-      </el-menu-item>
-      <el-submenu index="article" class="header-menu-item">
-        <template slot="title">看文章</template>
-        <el-menu-item :index="workDiaryUrl">
-          工作日志</el-menu-item>
-      </el-submenu>
-      <el-menu-item index="https://github.com/Bob2100/common-vue/tags"
-        class="header-menu-item">{{version}}
-      </el-menu-item>
-    </el-menu>
+    <MenuPc class="header-menu" />
     <div class="header-menu-more">
       <i class="el-icon-s-fold" @click="isShowDrawer = true"></i>
     </div>
@@ -37,8 +21,12 @@
 </template>
 
 <script>
+import MenuPc from './MenuPc.vue'
 export default {
   name: 'Header',
+  components: {
+    MenuPc
+  },
   data() {
     return {
       activeIndex: '',
@@ -73,9 +61,6 @@ export default {
     this.initActiveIndex()
   },
   methods: {
-    ss() {
-      console.log('sss');
-    },
     initActiveIndex() {
       this.activeIndex = this.currentPath.substring(1);
     },
--- src/views/layout/MenuPc.vue
+++ src/views/layout/MenuPc.vue
@@ -1,15 +1,67 @@
 <template>
-  <div>
-
-  </div>
+  <el-menu :default-active="active" class="MenuPc" mode="horizontal"
+    @select="handleSelect" background-color="#34495e" text-color="#fff"
+    active-text-color="#1abc9c">
+    <el-menu-item index="index">
+      <span class="logo" @click="$router.push({name:'Layout'})">前端二牛</span>
+    </el-menu-item>
+    <el-menu-item index="GitTeach" class="header-menu-item">Git常用命令
+    </el-menu-item>
+    <el-submenu index="article" class="header-menu-item">
+      <template slot="title">看文章</template>
+      <el-menu-item :index="workDiaryUrl">
+        工作日志</el-menu-item>
+    </el-submenu>
+    <el-menu-item index="https://github.com/Bob2100/common-vue/tags"
+      class="header-menu-item">{{version}}
+    </el-menu-item>
+  </el-menu>
 </template>
 
 <script>
-  export default {
-    
+export default {
+  data() {
+    return {
+      activeIndex: '',
+    }
+  },
+  computed: {
+    currentPath() {
+      return this.$route.path;
+    },
+    version() {
+      return `v${process.env.VUE_APP_VERSION}`;
+    },
+    workDiaryUrl() {
+      return window.workDiaryUrl;
+    }
+  },
+  watch: {
+    currentPath() {
+      this.initActiveIndex();
+    },
+    activeIndex(next) {
+      if (next.indexOf('http') === 0) {
+        this.$nextTick(() => this.initActiveIndex());
+        window.open(next);
+        return;
+      }
+      this.$router.push({ name: next })
+    }
+  },
+  created() {
+    this.initActiveIndex()
+  },
+  methods: {
+    initActiveIndex() {
+      this.activeIndex = this.currentPath.substring(1);
+    },
+    handleSelect(key) {
+      this.activeIndex = key
+    }
   }
+}
 </script>
 
 <style lang="scss" scoped>
-
 </style>
