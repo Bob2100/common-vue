ad92984cc7699fb8657973cc680a1db0869259c5
Bob <bob_w@qq.com>
2021-12-02 10:22:36 +0800

    分离出HeaderMobile组件

--- src/views/layout/Header.vue
+++ src/views/layout/Header.vue
@@ -1,72 +1,18 @@
 <template>
   <div class="Header">
     <MenuPc class="header-menu" />
-    <div class="header-menu-more">
-      <i class="el-icon-s-fold" @click="isShowDrawer = true"></i>
-    </div>
-    <el-drawer @click.native="isShowDrawer = false" :visible.sync="isShowDrawer"
-      :with-header="false" append-to-body>
-      <el-menu :default-active="activeIndex" class="header-menu"
-        @select="handleSelect" background-color="#34495e" text-color="#fff"
-        active-text-color="#1abc9c">
-        <el-menu-item index="GitTeach">Git常用命令
-        </el-menu-item>
-        <el-menu-item :index="workDiaryUrl">工作日志</el-menu-item>
-        <el-menu-item index="https://github.com/Bob2100/common-vue/tags">
-          {{version}}
-        </el-menu-item>
-      </el-menu>
-    </el-drawer>
+    <MenuMobile class="header-menu-more" />
   </div>
 </template>
 
 <script>
 import MenuPc from './MenuPc.vue'
+import MenuMobile from './MenuMobile.vue'
 export default {
   name: 'Header',
   components: {
-    MenuPc
-  },
-  data() {
-    return {
-      activeIndex: '',
-      isShowDrawer: false
-    };
-  },
-  computed: {
-    currentPath() {
-      return this.$route.path;
-    },
-    version() {
-      return `v${process.env.VUE_APP_VERSION}`;
-    },
-    workDiaryUrl() {
-      return window.workDiaryUrl;
-    }
-  },
-  watch: {
-    currentPath() {
-      this.initActiveIndex();
-    },
-    activeIndex(next) {
-      if (next.indexOf('http') === 0) {
-        this.$nextTick(() => this.initActiveIndex());
-        window.open(next);
-        return;
-      }
-      this.$router.push({ name: next })
-    }
-  },
-  created() {
-    this.initActiveIndex()
-  },
-  methods: {
-    initActiveIndex() {
-      this.activeIndex = this.currentPath.substring(1);
-    },
-    handleSelect(key) {
-      this.activeIndex = key
-    }
+    MenuPc,
+    MenuMobile
   }
 }
 </script>
@@ -83,15 +29,6 @@ export default {
 }
 .header-menu {
   flex: 1;
-}
-.el-icon-s-fold {
-  font-size: 36px;
-  line-height: 60px;
-  color: #fff;
-  transition: all 0.3s;
-  &:hover {
-    color: @green;
-  }
 }
 @media screen and (max-width: 900px) {
   .header-menu-item {
