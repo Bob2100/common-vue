17d7ce057f53488f0d4d2dee2fc09e1cc5ba5cd6
Bob <bob_w@qq.com>
2021-12-06 10:21:36 +0800

    雷达图

--- src/main.js
+++ src/main.js
@@ -4,12 +4,14 @@ import App from './App.vue'
 import './plugins/element.js'
 import router from './router'
 import store from './store'
+import BoCharts from './bo-charts'
 import BoUI from './bo-ui'
 import './style/index.less'
 import mixin from './mixin'
 
 Vue.config.productionTip = false;
 Vue.mixin(mixin);
+Vue.use(BoCharts);
 Vue.use(BoUI);
 new Vue({
   router,
