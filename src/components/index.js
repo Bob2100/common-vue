c685b9f8ea4258f0e5d551977ed0f330ca9ac997
Bob <bob_w@qq.com>
2022-01-06 20:54:32 +0800

    注册到全局

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,10 @@
 import BaseSearchBox from './BaseSearchBox.vue'
-
+const components = [BaseSearchBox]
+const install = function (Vue) {
+  components.map((component) => {
+    Vue.component(component.name, component)
+  })
+}
 export default {
-  install(Vue) {
-    components.map((component) => {
-      Vue.component(component.name, component)
-    })
-  },
+  install,
 }
