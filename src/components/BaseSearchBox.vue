ff3eb534a7e5199b0a5368b470f49151d84a3378
Bob <bob_w@qq.com>
2022-01-06 20:24:31 +0800

    BaseSearchBox组件

---
+++ src/components/BaseSearchBox.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="BaseSearchBox">
+    <el-input :value="value" @input="$emit('input', $event)" />
+    <el-button @click="$emit('search')" />
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    value: String
+  }
+}
+</script>
