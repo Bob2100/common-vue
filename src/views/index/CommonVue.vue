1056efa136fb25c1cb0565b9fb5bf1d291cb8e34
Bob <bob_w@qq.com>
2022-01-10 20:05:40 +0800

    首页更新

--- src/views/index/CommonVue.vue
+++ src/views/index/CommonVue.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="CommonVue">
-    <Faq />
+    <h1>欢迎访问二牛的小站</h1>
+    <img src="../../assets/erniu.jpeg" alt="公众号" />
+    <p>
+      <a href="https://github.com/Bob2100/common-vue" target="_blank"
+        >点击这里查看本站源码</a
+      >
+    </p>
   </div>
 </template>
 
 <script>
-import Faq from './Faq.vue'
 export default {
   name: 'CommonVue',
-  components: {
-    Faq,
-  },
 }
 </script>
-
-<style lang="scss" scoped></style>
