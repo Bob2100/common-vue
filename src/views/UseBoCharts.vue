a2555010639585e3b498c0eeddbe1bdb38433dc3
Bob <bob_w@qq.com>
2022-01-10 14:36:54 +0800

    修改示例

--- src/views/UseBoCharts.vue
+++ src/views/UseBoCharts.vue
@@ -9,15 +9,27 @@ export default {
   computed: {
     lineOption() {
       return {
+        title: {
+          text: '营业收入折线图',
+        },
+        legend: {
+          data: ['营业收入'],
+        },
+        tooltip: {
+          trigger: 'axis',
+        },
         xAxis: {
+          name: '年',
           type: 'category',
-          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+          data: ['2015', '2016', '2017', '2018', '2019', '2020', '2021'],
         },
         yAxis: {
           type: 'value',
+          name: '亿元',
         },
         series: [
           {
+            name: '营业收入',
             data: [150, 230, 224, 218, 135, 147, 260],
             type: 'line',
           },
