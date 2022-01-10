c1d1af80476076814e89cf44649e941091aef7b2
Bob <bob_w@qq.com>
2022-01-10 16:56:35 +0800

    修改图表示例

--- src/views/UseBoCharts.vue
+++ src/views/UseBoCharts.vue
@@ -5,6 +5,27 @@
     <div class="code-wrapper">
       <pre class="language-text"><code>npm i -S bo-charts</code></pre>
     </div>
+    <h2>在Vue2中注册 BoCharts</h2>
+    <p>你可以使用如下命令将 BoCharts 注册到 Vue</p>
+    <div class="code-wrapper">
+      <pre class="language-text"><code>import BoCharts from 'bo-charts' 
+Vue.use(BoCharts)</code></pre>
+    </div>
+    <h2>使用示例</h2>
+    <p>
+      下面示例中 option 的格式和
+      <a
+        href="https://echarts.apache.org/examples/zh/editor.html?c=line-simple"
+        target="_blank"
+        >echarts</a
+      >
+      官方文档保持一致
+    </p>
+    <div class="code-wrapper">
+      <pre
+        class="language-text"><code>&lt;bo-chart v-bind="option" /&gt;</code></pre>
+    </div>
+    <h2>展示效果</h2>
     <bo-chart v-bind="lineOption" />
   </div>
 </template>
